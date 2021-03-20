92e1ddbd70f7cdda5464c710fb6a23825faa9daf
Freedzio <luniewski.tomasz@gmail.com>
2021-03-20 22:19:51 +0100

    auto push

--- commands/timer.js
+++ commands/timer.js
@@ -1,6 +1,6 @@
 module.exports = function (msg, args) {
   console.log(args)
-  const unit = args.toString().split().pop()
+  const unit = args.join().split('').pop()
   console.log(unit)
   msg.reply(unit.toString())
 }
