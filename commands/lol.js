69b031d822e4654c591efdebfb2487e00b0ec369
Freedzio <luniewski.tomasz@gmail.com>
2021-03-25 22:05:07 +0100

    auto push

--- commands/lol.js
+++ commands/lol.js
@@ -16,38 +16,43 @@ module.exports = async function (msg, args) {
     });
 
     const summonerData = await summonerResponse.json();
+    // console.log(summonerData);
 
-    const { id, accountId, puuid, name, profileIconId, summonerLevel } = summonerData;
-
-    const summonerRankedResponse = await fetch(`${baseURL}/lol/league/v4/entries/by-summoner/${id}`, {
-      headers: {
-        "X-Riot-Token": apiKey
-      }
-    })
-
-    const summonerRankedData = await summonerRankedResponse.json();
-
-    const summonerEmbed = new MessageEmbed()
-      .setTitle(`Staty summonera ${name}`)
-      .setColor('DARK_BLUE')
-      .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/${profileIconId}.png`)
-      .addField('Poziom', summonerLevel)
-    if (summonerRankedData.length > 0) {
-
-
-      for (let i = 0; i < summonerRankedData.length; i++) {
-        const { tier, rank, wins, losses, queueType } = summonerRankedData[i];
-
-        summonerEmbed
-          .addField(queueTypes[queueType], `${tier} ${rank}`)
-          .addField('Wygrane', wins, true)
-          .addField('Przegrane', losses, true)
-      }
-
+    if (summonerData.status && summonerData.status.status_code === 404) {
+      msg.reply('ni mo takiego summonera')
     } else {
-      summonerEmbed.setDescription('Ni mo rankingu żadnego')
+      const { id, accountId, puuid, name, profileIconId, summonerLevel } = summonerData;
+
+      const summonerRankedResponse = await fetch(`${baseURL}/lol/league/v4/entries/by-summoner/${id}`, {
+        headers: {
+          "X-Riot-Token": apiKey
+        }
+      })
+
+      const summonerRankedData = await summonerRankedResponse.json();
+
+      const summonerEmbed = new MessageEmbed()
+        .setTitle(`Staty summonera ${name}`)
+        .setColor('DARK_BLUE')
+        .setThumbnail(`http://ddragon.leagueoflegends.com/cdn/11.6.1/img/profileicon/${profileIconId}.png`)
+        .addField('Poziom', summonerLevel)
+      if (summonerRankedData.length > 0) {
+
+        for (let i = 0; i < summonerRankedData.length; i++) {
+          const { tier, rank, wins, losses, queueType } = summonerRankedData[i];
+
+          summonerEmbed
+            .addField(queueTypes[queueType], `${tier} ${rank}`)
+            .addField('Wygrane', wins, true)
+            .addField('Przegrane', losses, true)
+        }
+
+      } else {
+        summonerEmbed.setDescription('Ni mo rankingu żadnego')
+      }
+
+      msg.reply(summonerEmbed)
     }
-    msg.reply(summonerEmbed)
 
   } catch (e) {
     msg.reply('coś nie siadło')
