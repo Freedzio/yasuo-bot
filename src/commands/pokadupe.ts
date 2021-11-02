07bfecb1131df3fb1864c8e0942731603e778df3
Freedzio <luniewski.tomasz@gmail.com>
2021-11-02 14:42:22 +0100

    auto push

--- src/commands/pokadupe.ts
+++ src/commands/pokadupe.ts
@@ -9,6 +9,7 @@ const replies = [
   "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] ok, ale nie jestem tani",
   "(____  .  ____)",
   "moje ciało jest gotowe",
+  "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)",
 ];
 export function pokadupe(msg: Message) {
   // args[0] === 'on' ? active = true && msg.reply('moje ciało jest gotowe')
