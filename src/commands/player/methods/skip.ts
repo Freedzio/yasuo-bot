c80fa79c90b6983c88641c918754dd7a7b434cf4
Freedzio <luniewski.tomasz@gmail.com>
2021-12-13 19:34:09 +0100

    auto push

--- src/commands/player/methods/skip.ts
+++ src/commands/player/methods/skip.ts
@@ -2,10 +2,16 @@ import { Message } from "discord.js";
 import { getServerQueue } from "../getServerQueue";
 
 export function skip(message: Message) {
-  if (!message.member.voice.channel)
+  const voiceChannel = message.member.voice.channel;
+
+  if (!voiceChannel)
     return message.channel.send(
       "Musisz być na kanale głosowym aby pomijać utwory!"
     );
+
+  if (!voiceChannel.members.some((m) => m.id === message.author.id))
+    return message.reply("Jesteś na innym kanale kasztaniarzu");
+
   if (!getServerQueue(message))
     return message.channel.send("Nie ma czego pomijać!");
   getServerQueue(message).connection.dispatcher.end();
--- src/commands/player/methods/stop.ts
+++ src/commands/player/methods/stop.ts
@@ -2,10 +2,15 @@ import { Message } from "discord.js";
 import { getServerQueue } from "../getServerQueue";
 
 export function stop(message: Message) {
-  if (!message.member.voice.channel)
+  const voiceChannel = message.member.voice.channel;
+
+  if (!voiceChannel)
     return message.channel.send(
       "Musisz być na kanale głosowym aby zatrzymać playlistę!"
     );
+
+  if (!voiceChannel.members.some((m) => m.id === message.author.id))
+    return message.reply("Jesteś na innym kanale kasztaniarzu");
 
   if (!getServerQueue(message))
     return message.channel.send("Nie ma czego zatrzymywać!");
