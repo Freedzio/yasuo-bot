97032f9fe08b45981396a3d6b845b0061dfa8c5c
Freedzio <luniewski.tomasz@gmail.com>
2022-04-17 21:09:56 +0200

    auto push

--- src/commands/pokadupe.ts
+++ src/commands/pokadupe.ts
@@ -1,20 +1,21 @@
-import { Message } from "discord.js";
-import { getRandomItem } from "../helpers";
+import { Message } from 'discord.js';
+import { getRandomItem } from '../helpers';
 
 const replies = [
-  "( ͡° ͜ʖ ͡°) chciałbyś",
-  "( ͡° ͜ʖ ͡°)╭∩╮ nie",
-  "( ͠° ͟ʖ ͡°) słucham?",
-  "(° ͜ʖ °) dupy to se możesz na Morence zobaczyć",
-  "[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] ok, ale nie jestem tani",
-  "(____  .  ____)",
-  "moje ciało jest gotowe",
-  "( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)",
+	'( ͡° ͜ʖ ͡°) chciałbyś',
+	'( ͡° ͜ʖ ͡°)╭∩╮ nie',
+	'( ͠° ͟ʖ ͡°) słucham?',
+	'(° ͜ʖ °) dupy to se możesz na Morence zobaczyć',
+	'[̲̅$̲̅(̲̅ ͡° ͜ʖ ͡°̲̅)̲̅$̲̅] ok, ale nie jestem tani',
+	'(____  .  ____)',
+	'moje ciało jest gotowe',
+	'( ͡°( ͡° ͜ʖ( ͡° ͜ʖ ͡°)ʖ ͡°) ͡°)',
+	'No zertwij to ze mnie'
 ];
 export function pokadupe(msg: Message) {
-  // args[0] === 'on' ? active = true && msg.reply('moje ciało jest gotowe')
-  //   : args[0] === 'off' ? active = false && msg.reply('koniec psot')
-  //     : active &&  msg.reply(helpers.getRandomItem(replies))
+	// args[0] === 'on' ? active = true && msg.reply('moje ciało jest gotowe')
+	//   : args[0] === 'off' ? active = false && msg.reply('koniec psot')
+	//     : active &&  msg.reply(helpers.getRandomItem(replies))
 
-  msg.reply(getRandomItem(replies));
+	msg.reply(getRandomItem(replies));
 }
